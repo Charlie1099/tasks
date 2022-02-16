a0656bef31490264d1c8b2323773bf7e8b35e39c
charlie1099 <cdcharlesworth@gmail.com>
2022-02-15 21:27:15 -0700

    worked out db issues

--- config/dev.exs
+++ config/dev.exs
@@ -3,7 +3,7 @@ import Config
 # Configure your database
 config :tasks, Tasks.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "1099758)1569AAc",
   hostname: "localhost",
   database: "tasks_dev",
   show_sensitive_data_on_connection_error: true,
